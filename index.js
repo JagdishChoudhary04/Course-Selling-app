cb492e5ece0439615c92592b9ad451832df63144
Jagdish Choudhary <jagdishchodhary426@gmail.com>
2025-09-20 11:07:40 +0530

    Add Skeleton Routes

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-console.log("Hii");
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req,res) => {
+    res.send("Main Page")
+})
+
+app.post('/user/login', function (req,res) {
+    res.json({
+        message: "Login endpoint"
+    })
+})
+
+app.post('user/signup', function (req,res){
+    res.json({
+        message: "Sign Up endpoint"
+    })
+})
+
+app.post('course/purchase', function (req,res) {
+    res.send("Buy Courses")
+})
+
+app.get('/courses', function (req,res) {
+    res.send("All Courses")
+})
+
+app.get('user/purchases', function (req,res) {
+    res.send("My Purchased Courses")
+})
+
+app.listen(port, () => {
+    console.log('Example port listening on port', port)
+})
